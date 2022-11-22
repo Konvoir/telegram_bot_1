39249ea0a7d77b57e4fa0a2ce3fe57d0b815f997
Konvoir <60233103+Konvoir@users.noreply.github.com>
2022-11-23 00:03:57 +0200

    кнопки

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const bot = new TelegramApi(token, { polling: true })
 
 const chats = {}
 
+const gameOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{ text: '1', callback_data: '1' }, { text: '2', callback_data: '2' }, { text: '3', callback_data: '3' }],
+            [{ text: '4', callback_data: '4' }, { text: '5', callback_data: '5' }, { text: '6', callback_data: '6' }],
+            [{ text: '7', callback_data: '7' }, { text: '8', callback_data: '8' }, { text: '9', callback_data: '9' }],
+            [{ text: '0', callback_data: '0' }],
+        ]
+    })
+}
 
 const start = () => {
     bot.setMyCommands([
@@ -33,10 +43,22 @@ const start = () => {
             await bot.sendMessage(chatId, 'Сейчас я загадаю цифру от 0 до 9, а ты попробуй угадать!')
             const randomNumber = Math.floor(Math.random() * 10)
             chats[chatId] = randomNumber;
-            return bot.sendMessage(chatId, 'Отгадывай');
+            return bot.sendMessage(chatId, 'Отгадывай', gameOptions);
         }
+        return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз!');
 
-        return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз!')
+    })
+
+    bot.on('callback_query', async msg => {
+        const data = msg.data;
+        const chatId = msg.message.chat.id;
+        if (data === chats[chatId]) {
+            return await bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]}`)
+        } else {
+            return await bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`)
+        }
+        // bot.sendMessage(chatId, `Ты выбрал цифру ${data}`)
+        // console.log(msg)
     })
 }
 
